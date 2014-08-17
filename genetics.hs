e36d152895511f6271eeaa1f4b916c2b6327beed
Noel Markham <noel.markham@gmail.com>
2014-08-17 22:35:37 +0100

    Added a function for picking a weighted value from a population

--- genetics.hs
+++ genetics.hs
@@ -1,14 +1,22 @@
 module Genetics where
 
 import Data.List (inits)
+import Data.Fixed (mod')
 
 type Chromasome = [Bool]
 
+roulettePick :: [(Chromasome, Double)] -> Double -> (Chromasome, Double)
+roulettePick population value = head $ dropWhile (\x -> (snd x) < normValue) roulettePop
+     where roulettePop = createRouletteProportions population
+           normValue = mod' value 1.0
+
 createRouletteProportions :: [(Chromasome, Double)] -> [(Chromasome, Double)]
-createRouletteProportions population =  zip chromasomes runningTotals
+createRouletteProportions population =  zip chromasomes normTotals
      where chromasomes = fmap fst population
            scores = fmap (\(_, s) -> 1 / s) population
            runningTotals = fmap sum $ tail $ inits scores
+           max = last runningTotals
+           normTotals = fmap (\x -> x / max) runningTotals
 
 crossover :: Int -> (Chromasome, Chromasome) -> (Chromasome, Chromasome)
 crossover splitPoint (one, two) = (oneA ++ twoB, twoA ++ oneB)
