2b3a67b66d0b887ce9549a4249ed07faacbe3fce
Noel Markham <noel.markham@gmail.com>
2014-08-19 21:37:35 +0100

    Returning the original score rather than the weighted score

--- genetics-tests.hs
+++ genetics-tests.hs
@@ -3,6 +3,8 @@ import Text.Printf
 import Genetics
 import Data.List (nub)
 
+
+
 ----------- doMutation
 
 prop_doMutation_length bits chromasome = length (doMutation bits chromasome) == length chromasome
@@ -27,16 +29,9 @@ prop_crossoverCrossover elements index = (crossedover . crossedover) pair == pai
 
 ----------- roulette choice
 
--- ([(a, Int)] -> [(a, Int)]) -> Int -> [(a, Int)] -> (a, Int)
-
 prop_createRouletteProportions_order population = createdProportions == original
     where original = fmap fst population
-          createdProportions = fmap fst $ createRouletteProportions population
-
-prop_createRouletteProportions_scoreIncreases prelimPopulation = all (\(a, b) -> a <= b) slidingScores
-    where population = fmap (\(a, b) -> (a, abs b)) prelimPopulation
-          scores = fmap snd $ createRouletteProportions population
-          slidingScores = zip scores $ tail scores
+          createdProportions = fmap (\(x, _, _) -> x) $ createRouletteProportions population
 
 -----------
 
@@ -49,8 +44,7 @@ tests = [
          ("crossover keeps chromasomes the same length",  quickCheck prop_crossover_length),
          ("crossover.crossover reurns the original pair", quickCheck prop_crossoverCrossover),
 
-         ("createRouletteProportions preserves the order",                                        quickCheck prop_createRouletteProportions_order),
-         ("createRouletteProportions score only ever increases",                                  quickCheck prop_createRouletteProportions_scoreIncreases)
+         ("createRouletteProportions preserves the order",       quickCheck prop_createRouletteProportions_order)
         ]
 
 main = mapM_ (\(s, a) -> printf "%-40s: " s >> a) tests
--- genetics.hs
+++ genetics.hs
@@ -6,17 +6,18 @@ import Data.Fixed (mod')
 type Chromasome = [Bool]
 
 roulettePick :: [(Chromasome, Double)] -> Double -> (Chromasome, Double)
-roulettePick population value = head $ dropWhile (\x -> (snd x) < normValue) roulettePop
+roulettePick population value = (chromasome, score)
      where roulettePop = createRouletteProportions population
-           normValue = mod' value 1.0
+           normValue = value `mod'` 1.0
+           (chromasome, score, _) = head $ dropWhile (\(_, _, x) -> x < normValue) roulettePop
 
-createRouletteProportions :: [(Chromasome, Double)] -> [(Chromasome, Double)]
-createRouletteProportions population =  zip chromasomes normTotals
+createRouletteProportions :: [(Chromasome, Double)] -> [(Chromasome, Double, Double)]
+createRouletteProportions population =  zip3 chromasomes scores normTotals
      where chromasomes = fmap fst population
            scores = fmap (\(_, s) -> 1 / s) population
            runningTotals = fmap sum $ tail $ inits scores
            max = last runningTotals
-           normTotals = fmap (\x -> x / max) runningTotals
+           normTotals = fmap (\x -> x / max ) runningTotals
 
 crossover :: Int -> (Chromasome, Chromasome) -> (Chromasome, Chromasome)
 crossover splitPoint (one, two) = (oneA ++ twoB, twoA ++ oneB)
